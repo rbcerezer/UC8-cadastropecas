79930cf270c86db75448f2f2fe534723942ff100
rb.cerezer <rb.cerezer@gmail.com>
2021-09-12 22:17:49 -0300

    desafio 01

---
+++ cadastropecas.js
@@ -0,0 +1,31 @@
+var listadepecas = []
+
+console.log ('Quantidade de Peças')
+if (listadepecas.lenght <= 10){
+    console.log ('Cadastro liberado')
+}
+else console.log ('Caixa Completa')
+
+//acredito ser uma outra opção de adicionar as peças até atingir o número máximo
+/*
+var pecas = 1
+while (pecas <=10){
+
+    console.log ('Cadastro liberado')
+    pecas = pecas + 1
+}
+*/
+
+console.log ('Nome da Peça')
+var nomepeca = []
+if (nomepeca >3){
+    console.log ('Cadastre o peso')
+}
+else console.log ('O nome da Peça deve ter mais de 3 caracteres')
+
+console.log ('Qual o peso da peça?')
+var pesopeca = []
+if (pesopeca >= 100){
+    console.log ('Cadastro realizado com sucesso!')
+}
+else console.log ('O peso da peça deve ser superior a 100g')
